6515a1ec2dfaa10b5af0611ad6647cfb30921ee2
DhairyaJain10 <73747192+DhairyaJain10@users.noreply.github.com>
2020-12-19 17:27:32 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,6 @@
+class Dustbin{
+    constructor(x,y)
+     Doptions = {
+         isStatic:false,friction:0.3,restitution:0.9,density:0.5
+    }
+}
---
+++ paper.js
@@ -0,0 +1,7 @@
+class Paper{
+    constructor(x,y)
+     Poptions = {
+         isStatic:false,friction:0.5,restitution:0.3,density:1.2
+    }
+    
+}
